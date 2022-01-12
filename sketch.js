ce1c21d8776542222b9fbd293964dc0640f4b234
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2022-01-12 05:50:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function preload(){
   
   jumpSound = loadSound("jump.mp3")
   dieSound = loadSound("die.mp3")
-  checkPointSound = loadSound("checkPoint.mp3")
+  checkPointSound = loadSound("checkpoint.mp3")
 }
 
 function setup() {
